986e7d5662c6a46ac079613dabbea3aaacb6d502
getdonked <stenyaev.da@gmail.com>
2025-03-10 22:11:02 +0400

    Create Power.java

---
+++ src/5/functions/meta/Power.java
@@ -0,0 +1,64 @@
+package 5.functions.meta;
+
+import 5.functions.Function;
+
+/**
+ * Класс Power
+ * Объекты данного класса представляет собой функцию, являющеюся степенью другой функции
+ * */
+public class Power implements Function {
+
+    /**
+     * Основание
+     * */
+    private Function base;
+
+    /**
+     * Показатель
+     * */
+    private Function exponent;
+
+    /**
+     * Левая граница области определения
+     * */
+    private double leftDomainBorder;
+
+    /**
+     * Правая граница области определения
+     * */
+    private double rightDomainBorder;
+
+    /**
+     * Конструктор
+     * */
+    public Power(Function base, Function exponent) {
+        this.base=base;
+        this.exponent=exponent;
+        leftDomainBorder=base.getLeftDomainBorder();
+        rightDomainBorder=base.getRightDomainBorder();
+    }
+
+    /**
+     * Метод получения левой границы области определения
+     * */
+    public double getLeftDomainBorder()
+    {
+        return leftDomainBorder;
+    }
+
+    /**
+     * Метод получения правой границы области определения
+     * */
+    public double getRightDomainBorder()
+    {
+        return rightDomainBorder;
+    }
+
+    /**
+     * Метод получения значения функции
+     * */
+    public double getFunctionValue(double x)
+    {
+        return Math.pow(base.getFunctionValue(x),exponent.getFunctionValue(x));
+    }
+}
