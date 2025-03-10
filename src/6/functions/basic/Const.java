6880b31f63149e72bd4f51f910627f085f6f9ee6
getdonked <stenyaev.da@gmail.com>
2025-03-10 22:31:52 +0400

    Create Const.java

---
+++ src/6/functions/basic/Const.java
@@ -0,0 +1,29 @@
+package 6.functions.basic;
+
+/**
+ * Класс Const описывает функцию, которая является константой
+ * например: y=C или y = 1
+ * */
+public class Const extends ContFunctions {
+
+    /**
+     * Сама константа
+     * */
+    private double y;
+
+    /**
+     * Конструктор
+     * */
+    public Const(double y)
+    {
+        this.y = y;
+    }
+
+    /**
+     * Определим метод получения значения функции
+     * */
+    public double getFunctionValue(double x)
+    {
+        return y;
+    }
+}
