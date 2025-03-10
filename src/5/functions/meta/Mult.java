5dc1274ec0561047b33f2d4c96dcbcf1220c63ae
getdonked <stenyaev.da@gmail.com>
2025-03-10 22:10:34 +0400

    Create Mult.java

---
+++ src/5/functions/meta/Mult.java
@@ -0,0 +1,58 @@
+package 5.functions.meta;
+
+import 5.functions.Function;
+
+/**
+ * Класс Mult
+ * Объекты данного класса, описывают произведение двух исходных функций
+ * */
+public class Mult implements Function {
+
+    /**
+     * Первая функция
+     * */
+    private Function first;
+
+    /**
+     * Вторая функция
+     * */
+    private Function second;
+
+    /**
+     * Левая граница области определения
+     * */
+    private double leftDomainBorder;
+
+    /**
+     * Правая граница области определения
+     * */
+    private double rightDomainBorder;
+
+    /**
+     * Конструктор
+     * */
+    public Mult(Function first, Function second) {
+        this.first=first;
+        this.second=second;
+        leftDomainBorder=Math.max(first.getLeftDomainBorder(), second.getLeftDomainBorder());
+        rightDomainBorder=Math.min(first.getRightDomainBorder(),second.getRightDomainBorder());
+    }
+
+    /**
+     *
+     * */
+    public double getLeftDomainBorder()
+    {
+        return leftDomainBorder;
+    }
+
+    public double getRightDomainBorder()
+    {
+        return rightDomainBorder;
+    }
+
+    public double getFunctionValue(double x)
+    {
+        return first.getFunctionValue(x)*second.getFunctionValue(x);
+    }
+}
