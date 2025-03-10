ced1abbac36857bf4c3349f72ca76d12bae81f0c
getdonked <stenyaev.da@gmail.com>
2025-03-10 22:05:11 +0400

    Create Exp.java

---
+++ src/5/functions/basic/Exp.java
@@ -0,0 +1,19 @@
+package 5.functions.basic;
+
+
+/**
+ * Класс Exp
+ * Объекты, данного класса вычисляют значение экспоненты
+ * Данный класс, расширяет ранее описанный общий класс ContFunctions, который реализует интерфейс Function
+ * */
+public class Exp extends ContFunctions{
+
+    /**
+     * Переопределим метод получения значения функции
+     * */
+    public double getFunctionValue(double x)
+    {
+        return Math.exp(x);
+    }
+
+}
