e9cf3260647624d0a0a083e691f1440bd4254184
getdonked <stenyaev.da@gmail.com>
2025-03-10 21:46:44 +0400

    Create TabulatedFunction.java

---
+++ src/4/functions/TabulatedFunction.java
@@ -0,0 +1,22 @@
+package 4.functions;
+
+public interface TabulatedFunction extends Function{
+
+    int getPointsCount();
+
+    FunctionPoint getPoint(int index);
+
+    double getPointX(int index);
+
+    double getPointY(int index);
+
+    void setPoint(int index, FunctionPoint point) throws InappropriateFunctionPointException;
+
+    void setPointX(int index, double x) throws InappropriateFunctionPointException;
+
+    void setPointY(int index, double y);
+
+    void addPoint(FunctionPoint point) throws InappropriateFunctionPointException;
+
+    void deletePoint(int index);
+}
