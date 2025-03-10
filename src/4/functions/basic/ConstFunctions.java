9833f6af0ae48dffaea48e3c1f324a4ca62b83ac
getdonked <stenyaev.da@gmail.com>
2025-03-10 21:27:39 +0400

    Create ConstFunctions.java

---
+++ src/4/functions/basic/ConstFunctions.java
@@ -0,0 +1,33 @@
+package 4.functions.basic;
+
+import 4.functions.Function;
+
+/**
+ * Класс описывает бесконечно дифференцируемые функции определённые на промежутке (-inf, +inf)
+ * Данный класс будет базовым для функций Log (логарифм), Exp (экспонента), Const(константа):
+ * Это означает что в классе мы определяем общие параметры функций, и для частного случая будем просто переопределять метод,
+ * по которому будем получать значение функции или, например, метод получения области определения
+ */
+public abstract class ContFunctions implements Function {
+
+    /**
+     * Определим левую границу функции
+     */
+    public double getLeftDomainBorder() {
+        return Double.NEGATIVE_INFINITY;
+    }
+
+    /**
+     * Определим правую границу функции
+     */
+    public double getRightDomainBorder() {
+        return Double.POSITIVE_INFINITY;
+    }
+
+    /**
+     * Определим значение функции
+     */
+    public double getFunctionValue(double x) {
+        return 0.;
+    }
+}
