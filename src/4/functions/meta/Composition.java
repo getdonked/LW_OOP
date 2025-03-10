a808bcc8d306c2108320cd7965e0446e6e8a2493
getdonked <stenyaev.da@gmail.com>
2025-03-10 21:32:45 +0400

    Create Composition.java

---
+++ src/4/functions/meta/Composition.java
@@ -0,0 +1,64 @@
+package 4.functions.meta;
+
+import 4.functions.Function;
+
+/**
+ * Класс Composition
+ * Объекты данного класса, описывают композицию двух исходных функций
+ * */
+public class Composition implements Function {
+
+    /**
+     * Внутренняя функция
+     * */
+    private Function inner;
+
+    /**
+     * Внешняя функция
+     * */
+    private Function outer;
+
+    /**
+     * Левая граница области определения
+     * */
+    private double leftDomainBorder;
+
+    /**
+     * Правая граница области определения
+     * */
+    private double rightDomainBorder;
+
+    /**
+     * Конструктор
+     * */
+    public Composition(Function inner, Function outer) {
+        this.inner = inner;
+        this.outer = outer;
+        leftDomainBorder=inner.getLeftDomainBorder();
+        rightDomainBorder=inner.getRightDomainBorder();
+    }
+
+    /**
+     * Метод получения левой границы области определения
+     * */
+    public double getLeftDomainBorder()
+    {
+        return leftDomainBorder;
+    }
+
+    /**
+     * Метод получения правой границы области определения
+     * */
+    public double getRightDomainBorder()
+    {
+        return rightDomainBorder;
+    }
+
+    /**
+     * Метод получения значения функции
+     * */
+    public double getFunctionValue(double x)
+    {
+        return outer.getFunctionValue(inner.getFunctionValue(x));
+    }
+}
