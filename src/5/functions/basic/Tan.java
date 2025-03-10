b95e702538ac18ad74a758b4061fc83caa0c7baa
getdonked <stenyaev.da@gmail.com>
2025-03-10 22:07:31 +0400

    Create Tan.java

---
+++ src/5/functions/basic/Tan.java
@@ -0,0 +1,17 @@
+package 5.functions.basic;
+
+/**
+ * Класс Tan
+ * Объекты, данного класса вычисляют значение Тангенса
+ * Данный класс, расширяет ранее описанный общий класс TrigonometricFunction, который реализует интерфейс Function
+ * */
+public class Tan extends TrigonometricFunction {
+
+    /**
+     * Значение функции тангнеса в точке
+     * */
+    public double getFunctionValue(double x)
+    {
+        return Math.tan(x);
+    }
+}
