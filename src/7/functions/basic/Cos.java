863d33204da0e61ebd6aecc423188074c9253b03
getdonked <stenyaev.da@gmail.com>
2025-03-11 19:51:35 +0400

    Create Cos.java

---
+++ src/7/functions/basic/Cos.java
@@ -0,0 +1,17 @@
+package 7.functions.basic;
+
+/**
+ * Класс Cos
+ * Объекты, данного класса вычисляют значение Косинуса
+ * Данный класс, расширяет ранее описанный общий класс TrigonometricFunction, который реализует интерфейс Function
+ * */
+public class Cos extends TrigonometricFunction {
+
+    /**
+     * Значение функции косинуса в точке
+     * */
+    public double getFunctionValue(double x) {
+        return Math.cos(x);
+    }
+
+}
