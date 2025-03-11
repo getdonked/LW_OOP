35bdd435befa2ff6769bae9ce529bb540b2a7ad8
getdonked <stenyaev.da@gmail.com>
2025-03-11 20:08:38 +0400

    Create Integrator.java

---
+++ src/7/threads/Integrator.java
@@ -0,0 +1,49 @@
+package 7.threads;
+
+import 7.functions.Functions;
+
+public class Integrator extends Thread {
+
+    // Задание
+    private Task task;
+    // Семафор
+    private Semaphore semaphore;
+
+    // Конктруктор
+    public Integrator(Task task, Semaphore semaphore){
+        this.task=task;
+        this.semaphore=semaphore;
+    }
+
+    // Метод run
+    @Override
+    public void run() {
+        // определим переменные для работы
+        double sum;
+        double leftDomainBorder;
+        double rightDomainBorder;
+        double sampleStep;
+        // в цикле идем по заданиям
+        for (int i = 0; i < task.getTaskCount(); i++) {
+            try{
+                // устанавливаем семафор
+                semaphore.beginRead();
+                // получаем левую границу
+                leftDomainBorder=task.getLeftDomainBorder();
+                // получаем правую границу
+                rightDomainBorder=task.getRightDomainBorder();
+                // получаем шаг дискроетизации
+                sampleStep=task.getSampleStep();
+                // получаем сумму
+                sum = Functions.integrate(task.getFunction(), leftDomainBorder, rightDomainBorder, sampleStep);
+                // выводим сообщение
+                System.out.println("Result " + leftDomainBorder + " " + rightDomainBorder + " " + sampleStep + " " + sum);
+                // снимаем семафор
+                semaphore.endRead();
+            }catch (InterruptedException e){
+                System.out.println("Integrator stopped");
+                return;
+            }
+        }
+    }
+}
